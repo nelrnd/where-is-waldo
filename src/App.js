a4d576f538157ba9cd7035b4974d27a4f3f74026
nel <nel.renaudin@gmail.com>
2023-03-20 11:52:51 +0100

    Create and style  LevelCard and LevelsGrid

--- src/App.js
+++ src/App.js
@@ -2,11 +2,30 @@ import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import './styles/App.css';
 
+// Temp
+import LevelCard from './components/LevelCard';
+import LevelsGrid from './components/LevelsGrid';
+
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <main></main>
+      <main>
+        <LevelsGrid>
+          <LevelCard
+            title="Mountain"
+            imageUrl="https://wallpapers.com/images/hd/where-s-waldo-snow-mountain-jfcrg87pmobfoauq.jpg"
+          />
+          <LevelCard
+            title="Mountain"
+            imageUrl="https://wallpapers.com/images/hd/where-s-waldo-snow-mountain-jfcrg87pmobfoauq.jpg"
+          />
+          <LevelCard
+            title="Mountain"
+            imageUrl="https://wallpapers.com/images/hd/where-s-waldo-snow-mountain-jfcrg87pmobfoauq.jpg"
+          />
+        </LevelsGrid>
+      </main>
       <Footer />
     </div>
   );
---
+++ src/components/LevelsGrid.js
@@ -0,0 +1,7 @@
+import '../styles/LevelsGrid.css';
+
+const LevelsGrid = ({ children }) => {
+  return <div className="LevelsGrid">{children}</div>;
+};
+
+export default LevelsGrid;
