1052f1f02e29ba882ca80361595c1cdd97f54829
nel <nel.renaudin@gmail.com>
2023-03-20 15:55:53 +0100

    Handle opening and closing animation on ContextMenu

--- src/components/ContextMenu.js
+++ src/components/ContextMenu.js
@@ -1,9 +1,35 @@
+import { useEffect, useState } from 'react';
 import '../styles/ContextMenu.css';
 
 const ContextMenu = ({ isOpen, position, characters }) => {
+  const [animation, setAnimation] = useState('');
+  const [isHidden, setIsHidden] = useState(true);
+
+  useEffect(() => {
+    const opening = async () => {
+      setIsHidden(false);
+      setAnimation('opening');
+      await new Promise((r) => setTimeout(r, 300));
+      setAnimation('');
+    };
+
+    const closing = async () => {
+      setAnimation('closing');
+      await new Promise((r) => setTimeout(r, 300));
+      setIsHidden(true);
+      setAnimation('');
+    };
+
+    if (isOpen) {
+      opening();
+    } else {
+      closing();
+    }
+  }, [isOpen]);
+
   return (
     <div
-      className={'ContextMenu' + (!isOpen ? ' hidden' : '')}
+      className={`ContextMenu ${isHidden ? 'hidden' : ''} ${animation}`}
       style={{ left: position.x + 'px', top: position.y + 'px' }}
     >
       <ul>
