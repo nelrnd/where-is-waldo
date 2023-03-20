71ccc3860eac9c7313f34ca4f12764daac9d2eaa
nel <nel.renaudin@gmail.com>
2023-03-20 12:33:21 +0100

    Create and style Home page

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
+import Home from './pages/Home';
 import './styles/App.css';
-
-// Temp
-import LevelCard from './components/LevelCard';
-import LevelsGrid from './components/LevelsGrid';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <main>
-        <LevelsGrid>
-          <LevelCard
-            title="Mountain"
-            imageUrl="https://wallpapers.com/images/hd/where-s-waldo-snow-mountain-jfcrg87pmobfoauq.jpg"
-          />
-          <LevelCard
-            title="Mountain"
-            imageUrl="https://wallpapers.com/images/hd/where-s-waldo-snow-mountain-jfcrg87pmobfoauq.jpg"
-          />
-          <LevelCard
-            title="Mountain"
-            imageUrl="https://wallpapers.com/images/hd/where-s-waldo-snow-mountain-jfcrg87pmobfoauq.jpg"
-          />
-        </LevelsGrid>
-      </main>
+      <Home />
       <Footer />
     </div>
   );
