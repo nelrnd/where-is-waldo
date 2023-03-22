6395a18ce1df4269a53b799cf8ed078df9a325fd
Nel <nel.renaudin@gmail.com>
2023-03-22 09:20:09 +0100

    Create routes, add home link to NavBar brand

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Leaderboard from './pages/Leaderboard';
 import Level from './pages/Level';
 import './styles/App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <NavBar />
-      <Level />
-      <Leaderboard />
-      <Home />
-      <Footer />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/level/:levelid" element={<Level />} />
+          <Route path="/leaderboard/:levelid" element={<Leaderboard />} />
+        </Routes>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,19 @@
+import { Link } from 'react-router-dom';
 import '../styles/NavBar.css';
 
 const NavBar = ({ children }) => {
   return (
     <header className="NavBar">
       <div className="NavBar_content">
-        <div className="NavBar_brand">
-          <div className="NavBar_img-wrapper"></div>
-          <div className="NavBar_text-wrapper">
-            <span className="first-line">WHERE'S</span>
-            <span className="second-line">WALDO?</span>
+        <Link to="/">
+          <div className="NavBar_brand">
+            <div className="NavBar_img-wrapper"></div>
+            <div className="NavBar_text-wrapper">
+              <span className="first-line">WHERE'S</span>
+              <span className="second-line">WALDO?</span>
+            </div>
           </div>
-        </div>
+        </Link>
 
         {children}
       </div>
